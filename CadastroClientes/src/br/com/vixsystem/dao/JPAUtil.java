8f5e40bb9e07f903272d40ec62864a5bf6db77a7
Henrique <Henrique@192.168.25.2>
2014-10-02 17:59:14 -0300

    Não funciona o banco

--- CadastroClientes/src/br/com/vixsystem/dao/JPAUtil.java
+++ CadastroClientes/src/br/com/vixsystem/dao/JPAUtil.java
@@ -7,7 +7,7 @@ import javax.persistence.Persistence;
 public class JPAUtil {
 
 	private static EntityManagerFactory emf = Persistence
-			.createEntityManagerFactory("livraria");
+			.createEntityManagerFactory("CadastroClientes");
 
 	public EntityManager getEntityManager() {
 		return emf.createEntityManager();
--- CadastroClientes/src/br/com/vixsystem/dao/PopulaBanco.java
+++ CadastroClientes/src/br/com/vixsystem/dao/PopulaBanco.java
@@ -7,8 +7,6 @@ import java.util.Date;
 
 import javax.persistence.EntityManager;
 
-import br.com.caelum.livraria.modelo.Autor;
-import br.com.caelum.livraria.modelo.Livro;
 import br.com.vixsystem.model.Cliente;
 import br.com.vixsystem.model.Usuario;
 
@@ -20,54 +18,27 @@ public class PopulaBanco {
 
 		em.getTransaction().begin();
 
-		Usuario admin = geraUser("admin");
+		Usuario admin = geraUser(true,"admin","Administrador","admin");
 		em.persist(admin);
 
-		
-		Livro casmurro = geraLivro("978-8-52-504464-8", "Dom Casmurro",
-				"10/01/1899", 24.90, assis);
-		Livro memorias = geraLivro("978-8-50-815415-9",
-				"Memorias Postumas de Bras Cubas", "01/01/1881", 19.90, assis);
-		Livro quincas = geraLivro("978-8-50-804084-1", "Quincas Borba",
-				"01/01/1891", 16.90, assis);
-
-		em.persist(casmurro);
-		em.persist(memorias);
-		em.persist(quincas);
-
-		Livro alquemista = geraLivro("978-8-57-542758-3", "O Alquimista",
-				"01/01/1988", 19.90, coelho);
-		Livro brida = geraLivro("978-8-50-567258-7", "Brida", "01/01/1990",
-				12.90, coelho);
-		Livro valkirias = geraLivro("978-8-52-812458-8", "As Valkirias",
-				"01/01/1992", 29.90, coelho);
-		Livro maao = geraLivro("978-8-51-892238-9", "O Diario de um Mago",
-				"01/01/1987", 9.90, coelho);
-
-		em.persist(alquemista);
-		em.persist(brida);
-		em.persist(valkirias);
-		em.persist(maao);
-
-		Livro capitaes = geraLivro("978-8-50-831169-1", "Capitaes da Areia",
-				"01/01/1937", 6.90, amado);
-		Livro flor = geraLivro("978-8-53-592569-9",
-				"Dona Flor e Seus Dois Maridos", "01/01/1966", 18.90, amado);
-
-		em.persist(capitaes);
-		em.persist(flor);
-
+	
 		em.getTransaction().commit();
 		em.close();
 
 	}
 
-	private static Usuario geraUser(String nome) {
+	private static Usuario geraUser(boolean ativo, String login, String nomeCompleto,String senha){
 		Usuario user = new Usuario();
-		user.setNomeCompleto(nome);
+		
+		user.setNomeCompleto(nomeCompleto);
+		user.setAtivo(ativo);
+		user.setLogin(login);
+		user.setSenha(senha);
+		
 		return user;
 	}
 	
+	@SuppressWarnings("unused")
 	private static Cliente geraCliente(Long idUsuario, boolean ativo, String nome, String endereco, String tipo) {
 		
 		Cliente cliente = new Cliente();
@@ -93,3 +64,37 @@ public class PopulaBanco {
 	}
 
 }
+
+/*Usuario admin = geraLivro("978-8-52-504464-8", "Dom Casmurro",
+		"10/01/1899", 24.90, assis);
+Livro memorias = geraLivro("978-8-50-815415-9",
+		"Memorias Postumas de Bras Cubas", "01/01/1881", 19.90, assis);
+Livro quincas = geraLivro("978-8-50-804084-1", "Quincas Borba",
+		"01/01/1891", 16.90, assis);
+
+em.persist(casmurro);
+em.persist(memorias);
+em.persist(quincas);
+
+Livro alquemista = geraLivro("978-8-57-542758-3", "O Alquimista",
+		"01/01/1988", 19.90, coelho);
+Livro brida = geraLivro("978-8-50-567258-7", "Brida", "01/01/1990",
+		12.90, coelho);
+Livro valkirias = geraLivro("978-8-52-812458-8", "As Valkirias",
+		"01/01/1992", 29.90, coelho);
+Livro maao = geraLivro("978-8-51-892238-9", "O Diario de um Mago",
+		"01/01/1987", 9.90, coelho);
+
+em.persist(alquemista);
+em.persist(brida);
+em.persist(valkirias);
+em.persist(maao);
+
+Livro capitaes = geraLivro("978-8-50-831169-1", "Capitaes da Areia",
+		"01/01/1937", 6.90, amado);
+Livro flor = geraLivro("978-8-53-592569-9",
+		"Dona Flor e Seus Dois Maridos", "01/01/1966", 18.90, amado);
+
+em.persist(capitaes);
+em.persist(flor);
+*/
